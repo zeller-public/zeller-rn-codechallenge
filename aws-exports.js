87ed97d1143068c909a3454aec767fdf22859160
James Merritt <james.merritt@myzeller.com>
2025-08-19 21:15:13 +1000

    Update api key
    Signed-off-by: James Merritt <james.merritt@myzeller.com>

--- aws-exports.js
+++ aws-exports.js
@@ -5,7 +5,7 @@ const awsconfig = {
     "https://prrwjjssnvhpbcdwbcwx3nm3zm.appsync-api.ap-southeast-2.amazonaws.com/graphql",
   aws_appsync_region: "ap-southeast-2",
   aws_appsync_authenticationType: "API_KEY",
-  aws_appsync_apiKey: "da2-s2fzla2ewnekhi2f66oxjwlx5u",
+  aws_appsync_apiKey: "da2-d46dkkw5xnfbxkxkhi6twfb7re",
 };
 
 export default awsconfig;
